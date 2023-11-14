6389bd6a5ae88623d5d667c9ff9cba1ddabfdb55
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-14 17:09:00 +0900

    feat: 배지 도메인의 모델 및 핵심 로직 구현

---
+++ src/model/Badge.js
@@ -0,0 +1,21 @@
+import { OUTPUT, BADGES } from '../common/constants.js';
+
+class Badge {
+
+  #totalBenefit;
+
+  constructor(totalBenefit) {
+    this.#totalBenefit = totalBenefit;
+  }
+
+  assignBadge() {
+    for (const { badge, amount } of BADGES) {
+      if (this.#totalBenefit >= amount) {
+        return badge;
+      }
+    }
+    return OUTPUT.none;
+  }
+};
+
+export default Badge;
