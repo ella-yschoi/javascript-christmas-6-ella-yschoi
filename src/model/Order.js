69b2f409296a4ff2f9154b348c0f191d279941e9
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-15 22:04:48 +0900

    fix: 메뉴와 개수 유효성 검사 로직 보완 및 수정

--- src/model/Order.js
+++ src/model/Order.js
@@ -1,22 +1,57 @@
-import { TYPE, ERROR, EVENT } from '../common/constants.js';
+import { ERROR, EVENT, REGEX, TYPE } from '../common/constants.js';
+
+import { SPECIAL_CHARACTERS } from '../common/constants.js';
 import { throwError } from '../common/utils.js';
+import Menu from './Menu.js';
 
 class Order {
 
   #menus;
 
-  constructor(menus) {
-    this.#menus = menus;
-    this.validate();
+  constructor(orderDetails) {
+    this.#validateOrderDetails(orderDetails);
+    this.#menus = this.#createMenusFromOrderString(orderDetails);
+    this.#validateOrder();
   }
 
-  validate() {
-    this.validateMenuDuplication();
-    this.validateDrinkOnlyOrder();
-    this.validateTotalMenuCount();
+  getMenus() {
+    return this.#menus;
   }
 
-  validateMenuDuplication() {
+  #validateOrderDetails(orderDetailsString) {
+    const orders = orderDetailsString.split(SPECIAL_CHARACTERS.comma);
+
+    const isInvalidOrder = orders.some(order => {
+      return !order.match(REGEX.menu_item_format);
+    });
+
+    if (isInvalidOrder) {
+      throwError(ERROR.order);
+    }
+  }
+  
+  #isValidMenuName(menuName) {
+    return menuName.match(REGEX.non_spaced_string);
+  }
+  
+  #isValidMenuCount(menuCount) {
+    return menuCount !== '' && !Number.isNaN(Number(menuCount));
+  }
+
+  #createMenusFromOrderString(orderDetailsString) {
+    return orderDetailsString.split(SPECIAL_CHARACTERS.comma).map(item => {
+      const [name, count] = item.split(SPECIAL_CHARACTERS.hyphen).map(str => str.trim());
+      return new Menu(name, Number(count));
+    });
+  }
+
+  #validateOrder() {
+    this.#validateMenuDuplication();
+    this.#validateDrinkOnlyOrder();
+    this.#validateTotalMenuCount();
+  }
+  
+  #validateMenuDuplication() {
     const menuNames = this.#menus.map(menu => menu.getName());
     const hasDuplicates = new Set(menuNames).size !== menuNames.length;
   
@@ -26,19 +61,18 @@ class Order {
     return menuNames;
   }
 
-  validateDrinkOnlyOrder() {
-    const isOnlyDrink = this.#menus.every(menu => menu.getType() === TYPE.drink);
-  
-    if (isOnlyDrink) {
-      throwError(ERROR.order);
+  #validateDrinkOnlyOrder() {
+    const isDrinkOnly = this.#menus.every(menu => menu.getType() === TYPE.drink);
+    if (isDrinkOnly) {
+      throwError(ERROR.only_beverage);
     }
     return this.#menus;
   }
 
-  validateTotalMenuCount() {
+  #validateTotalMenuCount() {
     const totalCount = this.#menus.reduce((sum, menu) => sum + menu.getCount(), 0);
     if (totalCount > EVENT.max_order) {
-      throwError(ERROR.order);
+      throwError(ERROR.order_over);
     }
     return totalCount;
   }
