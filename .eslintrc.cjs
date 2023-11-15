d4fe18c619f3c66ba75950dc0763fe58322ae83e
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-15 22:15:59 +0900

    style: ESLint와 Prettier 내 불필요한 공백 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,11 +7,10 @@ export default {
   },
   extends: [
     'airbnb-base',
-    'prettier'
+    'prettier',
   ],
   overrides: [
     {
-      // 테스트 파일에는 규칙 비활성화
       files: ['tests/**/*.js'],
       rules: {
         'max-lines-per-function': 'off',
@@ -24,13 +23,9 @@ export default {
 		sourceType: 'module',
   },
   rules: {
-    // 들여쓰기 깊이 제한
     'max-depth': ['error', 2],
-    // 함수의 매개변수 개수 제한
     'max-params': ['error', 3],
-    // 함수의 길이 제한
     'max-lines-per-function': ['error', { max: 15 }],
-    // eslint-import/extensions 규칙 비활성화
     'import/extensions': 'off',
   },
 };
